ed1ce36c919ffdb90e4447dd2b689b0e9c8f096e
Alec James <alehero@berkeley.edu>
2023-04-24 17:35:49 -0700

    BFS

---
+++ dist_pcsr.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <upcxx/upcxx.hpp>
+// #include "butil.hpp"
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <unordered_set>
+#include <deque>
+#include "set_pma.hpp"
+
+using namespace std;
+
+// struct Insert {
+//     uint32_t from;
+//     uint32_t to;
+//     Insert(uint32_t from, uint32_t to) : from(from), to(to) {}
+// };
+
+class DistPCSR {
+    public:
+        DistPCSR(uint32_t size, uint64_t max_val);
+        uint32_t size();
+        uint32_t num_vertices; // Restrict this to power of 2
+        /* Inserts an edge from one vertex to another. Fails if either vertex does not exist. */
+        void insert_edge(uint32_t from, uint32_t to);
+        /* Queries if an edge exists. */
+        bool query_edge(uint32_t from, uint32_t to);
+        /* Edge list of a vertex. If the vertex does not exist, the list is empty. */
+        vector<uint32_t> edges(uint32_t from);
+        /* Returns the entire adjacency list representation of the graph. */
+        vector<pair<uint32_t, vector<uint32_t>>> adjacency_lists();
+        // deque<Insert> rq_queue;
+
+        static uint64_t make_edge_tuple(uint32_t from, uint32_t to);
+        static pair<uint32_t, uint32_t> get_edge_tuple(uint64_t edge);
+        uint32_t target_rank(uint32_t from);
+        upcxx::dist_object<SetPMA> dist_spma;
+        const uint32_t TO_ONES = 0xFFFFFFFF;
+};
+
+
+DistPCSR::DistPCSR(uint32_t size, uint64_t max_val) : dist_spma(SetPMA(size)) {
+    num_vertices = max_val;
+}
+
+uint64_t DistPCSR::make_edge_tuple(uint32_t from, uint32_t to) { return (((uint64_t) from) << 32) | to; }
+pair<uint32_t, uint32_t> DistPCSR::get_edge_tuple(uint64_t edge) { return std::make_pair((uint32_t)(edge >> 32), (uint32_t)((edge << 32) >> 32)); } /* doesn't work */
+
+uint32_t DistPCSR::target_rank(uint32_t from) {
+    return from / (num_vertices / upcxx::rank_n());
+}
+
+void DistPCSR::insert_edge(uint32_t from, uint32_t to) {
+    uint32_t rank = target_rank(from);
+    uint64_t edge = make_edge_tuple(from, to);
+    upcxx::rpc(rank,
+        [](upcxx::dist_object<SetPMA> &dist_spma, uint64_t edge) {
+            dist_spma->insert(edge);
+        },
+        dist_spma, edge
+    ).wait();
+}
+
+bool DistPCSR::query_edge(uint32_t from, uint32_t to) {
+    uint32_t rank = target_rank(from);
+    uint64_t edge = make_edge_tuple(from, to);
+    return upcxx::rpc(rank, 
+        [](upcxx::dist_object<SetPMA>& dist_spma, uint64_t edge) {
+            return dist_spma->query(edge);
+        },
+        dist_spma, edge
+    ).wait();
+}
+
+vector<uint32_t> DistPCSR::edges(uint32_t from) {
+    uint32_t rank = target_rank(from);
+
+    return upcxx::rpc(rank,
+        [](upcxx::dist_object<SetPMA> &dist_spma, uint32_t from) {
+            uint64_t from_padded = ((uint64_t)from) << 32;
+            vector<uint32_t> edge_list;
+            auto edge_populater = SetPMA::range_func([&edge_list](uint64_t v) {
+                edge_list.push_back((uint32_t)v);
+            });
+
+            dist_spma->range(from_padded, from_padded | 0xFFFFFFFF, edge_populater);
+            return edge_list;
+        },
+        dist_spma, from
+    ).wait();
+}
+
+vector<pair<uint32_t, vector<uint32_t>>> DistPCSR::adjacency_lists() {
+    std::vector<upcxx::future<vector<pair<uint32_t, vector<uint32_t>>>>> futures;
+    for (int rank = 0; rank < upcxx::rank_n(); rank++) {
+        futures.push_back(upcxx::rpc(
+            rank,
+            [](upcxx::dist_object<SetPMA>& dist_spma) {
+                vector<pair<uint32_t, vector<uint32_t>>> adjacencies;
+                uint32_t curr_source = UINT32_MAX;
+                uint32_t* curr_source_ptr = &curr_source;
+                vector<uint32_t> adjacency;
+                auto f = SetPMA::range_func([&adjacencies, &adjacency, curr_source_ptr](uint64_t v) {
+                    uint32_t source, dest;
+                    source = v >> 32;
+                    dest = v;
+                    
+                    if (source != *curr_source_ptr) {
+                        if (*curr_source_ptr != UINT32_MAX) {
+                            adjacencies.push_back(
+                                std::make_pair(*curr_source_ptr, std::move(adjacency))
+                            );
+                            adjacency.clear();
+                        }
+                        *curr_source_ptr = source;
+                        adjacency.push_back(dest);
+                    } else {
+                        adjacency.push_back(dest);
+                    }
+                });
+                dist_spma->range(0, UINT64_MAX - 1, f);
+                    
+                // handle tail
+                if (curr_source != UINT32_MAX) {
+                    adjacencies.push_back(
+                        std::make_pair(curr_source, std::move(adjacency))
+                    );
+                }
+                return adjacencies;
+            }, dist_spma
+        ));
+    }
+    vector<pair<uint32_t, vector<uint32_t>>> combined_adj_list;
+    for (const auto& future : futures) {
+        const auto& adj_list = future.wait();
+        combined_adj_list.insert(combined_adj_list.end(), adj_list.begin(), adj_list.end());
+    }
+    return combined_adj_list;
+}
--- main_dist_pcsr.cpp
+++ main_dist_pcsr.cpp
@@ -10,15 +10,19 @@
 #include <fstream> 
 #include <sstream>
 #include <iostream>
+#include <deque>
 
-#include "distr_pcsr.hpp"
+#include "dist_pcsr.hpp"
 // #include "butil.hpp"
 
 using namespace std;
 
+vector<int> bfs(DistPCSR &pcsr, uint32_t source);
+
 int main(int argc, char** argv) {
     upcxx::init();
     ifstream infile;
+    ofstream outfile;
     
     if (argc < 2) {
         infile.open("dist_pcsr_inserts.txt");
@@ -35,7 +39,7 @@ int main(int argc, char** argv) {
     }
 
     if (argc >= 3) {
-        outfile.open(argv[2] + "_" + std::to_string(upcxx::rank_me()) + ".dat");
+        outfile.open(std::string(argv[2]) + "_" + std::to_string(upcxx::rank_me()) + ".dat");
         if (!outfile.is_open()) {
             cerr << "Couldn't open " << argv[2] << endl;
             return -1; 
@@ -47,12 +51,30 @@ int main(int argc, char** argv) {
     string line;
 
     DistPCSR pcsr(1 << 4, 16384);
-    line_number = 0;
+    int line_number = 0;
+
+    chrono::high_resolution_clock::time_point start_time, end_time;
+    std::chrono::duration<double> insert_duration(0), query_duration(0);
+
     while (getline(infile, line)) {
         istringstream iss(line);
         string command;
         iss >> command;
-        if (command == "START_INSERTS" || command == "START_QUERIES") {
+        if (command == "START_INSERTS") {
+            if (line_number != 0) {
+                end_time = std::chrono::high_resolution_clock::now();
+                query_duration += end_time - start_time;
+            }
+            start_time = std::chrono::high_resolution_clock::now();
+            upcxx::barrier();
+            line_number++;
+            continue;
+        } else if (command == "START_QUERIES") {
+            if (line_number != 0) {
+                end_time = std::chrono::high_resolution_clock::now();
+                insert_duration += end_time - start_time;
+            }
+            start_time = std::chrono::high_resolution_clock::now();
             upcxx::barrier();
             line_number++;
             continue;
@@ -70,11 +92,11 @@ int main(int argc, char** argv) {
             iss >> u >> v;
             bool exists = pcsr.query_edge(u, v);
             if (exists) {
-                outfile << "True" << endl;
+                outfile << u << " " << v << " True" << endl;
             } else {
-                outfile << "False" << endl;
+                outfile << u << " " << v << " False" << endl;
             }
-            outfile << endl;
+            // outfile << endl;
         } else if (command == "GET_OUT_EDGES") {
             uint32_t vertex;
             iss >> vertex;
@@ -90,7 +112,7 @@ int main(int argc, char** argv) {
                 }
                 outfile << endl;
             }
-            outfile << endl;
+            // outfile << endl;
         } else if (command == "QUERY_ALL_GRAPH") {
             vector<pair<uint32_t, vector<uint32_t>>> result = pcsr.adjacency_lists();
             if (write_output) {
@@ -102,15 +124,151 @@ int main(int argc, char** argv) {
                     }
                     outfile << endl;
                 }
-                outfile << endl;
+                // outfile << endl;
             }
         } else {
             cerr << "Received unsupported command: " << command << endl;
         }
+        line_number++;
     }
-
+    end_time = std::chrono::high_resolution_clock::now();
+    query_duration += end_time - start_time;
+    upcxx::barrier();
+    if (upcxx::rank_me() == 0) {
+        cout << "Inserts total time: " << std::chrono::duration<double>(insert_duration).count() << endl;
+        cout << "Queries total time: " << std::chrono::duration<double>(query_duration).count() << endl;
+    }
     infile.close();
     outfile.close();
+    start_time = std::chrono::high_resolution_clock::now();
+    vector<int> distances = bfs(pcsr, 0);
+    end_time = std::chrono::high_resolution_clock::now();
+    if (upcxx::rank_me() == 0) {
+        cout << "BFS total time: " << std::chrono::duration<double>(end_time - start_time).count() << endl;
+    }
+    if (write_output) {
+        outfile.open(std::string(argv[2]) + "_" + std::to_string(upcxx::rank_me()) + ".bfs");
+        uint32_t start_vertex = pcsr.num_vertices / upcxx::rank_n() * upcxx::rank_me();
+        for (uint32_t i = 0; i < distances.size(); i++) {
+            uint32_t vertex = start_vertex + i;
+            outfile << vertex << " " << distances[i] << endl;
+        }
+        outfile.close();
+    }
     upcxx::finalize();
     return 0;
 }
+
+vector<int> bfs(DistPCSR &pcsr, uint32_t source) {
+    uint32_t start_vertex = pcsr.num_vertices / upcxx::rank_n() * upcxx::rank_me();
+    uint32_t end_vertex = start_vertex + pcsr.num_vertices / upcxx::rank_n();
+    uint32_t local_num_vertices = end_vertex - start_vertex;
+
+    vector<int> local_distances(local_num_vertices, -1);
+
+    // cout << "Checkpoint 1" << endl;
+
+    if (source >= start_vertex && source < end_vertex) {
+        local_distances[source - start_vertex] = 0;
+    }
+
+    upcxx::barrier();
+
+    deque<uint32_t> frontiers;
+    if (upcxx::rank_me() == pcsr.target_rank(source)) {
+        frontiers.push_back(source);
+    }
+    int level = 0;
+
+    upcxx::dist_object<vector<uint32_t>> next_set_recv{vector<uint32_t>()};
+
+    // cout << "Checkpoint 2" << endl;
+
+    while (true) {
+        // cout << "Checkpoint 2.1" << endl;
+
+        vector<uint32_t> next_set;
+        // cout << "Checkpoint 2.2" << endl;
+        bool is_frontier_empty = frontiers.empty();
+        // cout << "Checkpoint 2.3" << endl;
+        bool all_frontiers_empty = upcxx::reduce_all(is_frontier_empty, upcxx::op_fast_bit_and).wait();
+        // cout << "Checkpoint 2.4" << endl;
+        if (all_frontiers_empty) {
+            // cout << "Checkpoint 2.5" << endl;
+            break;
+        }
+
+        // cout << "Checkpoint 3" << endl;
+
+        for (uint32_t vertex : frontiers) {
+            
+
+            vector<uint32_t> neighbors = pcsr.edges(vertex);
+            next_set.insert(next_set.end(), neighbors.begin(), neighbors.end());
+
+            // for (uint32_t neighbor : neighbors) {
+            //     if (neighbor >= start_vertex && neighbor < end_vertex) {
+            //         if ((*local_distances)[neighbor - start_vertex] == -1) {
+            //             (*local_distances)[neighbor - start_vertex] = current_distance + 1;
+            //             global_queue.push_back(neighbor);
+            //         }
+            //     } else {
+            //         int target_rank = pcsr.target_rank(neighbor);
+            //         int dest_rank_local_index = neighbor - pcsr.target_rank(neighbor) * local_num_vertices
+            //         int neighbor_distance = local_distances->fetch().wait()
+            //     }
+            // }
+        }
+
+        // cout << "Checkpoint 4" << endl;
+
+        vector<vector<uint32_t>> vertex_destinations;
+        vertex_destinations.resize(upcxx::rank_n());
+
+        for (uint32_t next_vertex : next_set) {
+            int target_rank = pcsr.target_rank(next_vertex);
+            vertex_destinations[target_rank].push_back(next_vertex);
+        }
+
+        (*next_set_recv).clear();
+
+        // cout << "Checkpoint 5" << endl;
+
+        for (uint32_t target_rank = 0; target_rank < upcxx::rank_n(); target_rank++) {
+            if (target_rank == upcxx::rank_me()) {
+                (*next_set_recv).insert((*next_set_recv).end(), vertex_destinations[target_rank].begin(), vertex_destinations[target_rank].end());
+                continue;
+            }
+
+            upcxx::rpc(
+                target_rank, 
+                [](upcxx::dist_object<vector<uint32_t>> &next_set_recv, const vector<uint32_t>& vertices) {
+                    (*next_set_recv).insert((*next_set_recv).end(), vertices.begin(), vertices.end());
+                },
+                next_set_recv, vertex_destinations[target_rank]
+            ).wait();
+
+        }
+        frontiers.clear();
+
+        // cout << "Checkpoint 6" << endl;
+        for (uint32_t vertex : *next_set_recv) {
+            // cout << "Another checkpoint 1" << endl;
+            int val = local_distances[vertex - start_vertex];
+            // if (level == 0)
+            // cout << "Another checkpoint 2. Val is " << val << endl;
+            // cout << "vertex - start_vertex is" << vertex - start_vertex << endl;
+            if (local_distances[vertex - start_vertex] == -1) {
+                local_distances[vertex - start_vertex] = level + 1;
+                // cout << "Another checkpoint 3" << endl;
+                frontiers.push_back(vertex);
+                // cout << "Another checkpoint 4" << endl;
+            }
+        }
+
+        level++;
+    }
+    return local_distances;
+    // cout << "Checkpoint inf" << endl;
+}
+
